3714f811e87a2a7d9988674d5f5e5db92ab2a85a
tina.yu <tina.yu@DCSHG.TENTAMUS.COM>
2021-08-08 14:51:53 +0800

    string added for word file

--- src/main/java/msword/StrOnCustomWord.java
+++ src/main/java/msword/StrOnCustomWord.java
@@ -11,10 +11,17 @@ public class StrOnCustomWord {
     public final static String OUTPUT_CELL = "Output cells";
     public final static String CELL_REF = "Cell reference";
     public final static String FORMULA = "Formula";
+    public final static String FORMULA_USED =  "Formula used";
     public final static String ACCEPT_R ="Acceptable Range";
     public final static String CELL = "Cell";
+    public final static String RANGE_VALIDATION = "Range Validation";
+    public final static String TEST_CASE = "Test Cases";
     public final static String range_valid_txt = "The background color of the cell(s) is set to red (Hex code: #FF0000) if the value do not meet specification.";
-    public final static String instruction_txt ="Instructions: Input the values listing in the table, then verify the output results and calculated results in the output cells. Output and calculated result values shall be equals to the expected results. All the output shall be confirmed by an approved calculator.";
-    public final static String accep_criteria_txt ="Acceptance Criteria: The output results and calculated results meet expected results.　";
+    public final static String instruction_txt ="Instructions: Input the values listing in the table, then verify the output results in the output cells. All the values of output and calculated result shall be equals to the expected results. All the output shall be confirmed by an approved calculator.";
+    public final static String accep_criteria_txt ="Acceptance Criteria: The output results and calculated results meet expected results.";
+    public final static String IN_SPEC = "In spec";
     public final static String RESULT= "result";
+    public final static String GENERAL_FORMAT=  "General format";
+    public final static String NUMERIC_DECIMAL=  "Numeric, Decimal ";
+    public final static String INTEGERL="Integer";
 }
