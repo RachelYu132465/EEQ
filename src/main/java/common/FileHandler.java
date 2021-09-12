e917fcdca9e0622731c0d5fe4f3db5c1b5721e07
tina.yu <ehad1111111@gmail.com>
2021-09-13 01:34:53 +0800

    filehandle

--- src/main/java/common/FileHandler.java
+++ src/main/java/common/FileHandler.java
@@ -2,42 +2,91 @@ package common;
 
 import msexcel.Excel;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import javax.swing.filechooser.FileSystemView;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
+import java.util.*;
 
+import static common.StringProcessor.*;
 import static msexcel.Excel.getSheetsBykeywordsIgnoreCase;
 import static test.ExcelForRu.proj_path;
 
 public class FileHandler {
-    public static void main(String[] args) {
-        //check every subfolder under input path
+    public static final String proj_path = FileSystemView.getFileSystemView().getHomeDirectory().getPath() + "/";
 
+    public static boolean moveFileToSubFolder(String path, String subfolderName, File f) {
+        File theDir = new File(path + "/" + subfolderName);
+        if (!theDir.exists()){
+            theDir.mkdirs();
+        }
+        if (f.exists() && f.isFile()) {
+            f.renameTo(new File(path + "/" + subfolderName+ "/"+f.getName()));
+//            f.renameTo(new File("C:\\Users\\tina.yu\\Desktop\\biotin_and_Eng\\test1.xlsx"));
+
+            System.out.println("!!!"+path + "/" + subfolderName+ "/"+f.getName());
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+
+//File ff = new File("C:\\Users\\tina.yu\\Desktop\\RU\\RT30397.xlsx");
+//                Excel excel = Excel.loadExcel(ff);
+//load檔案後無法移動檔案
+//        excel.outputFile("RT30397");
+//        File fW = new File("C:\\Users\\tina.yu\\Desktop\\RU\\RT30397.xlsx");
+//                moveFileToSubFolder(proj_path,"biotin_and_Eng",fW);
+////                System.out.println(excel.getFileName());
+//
+////                List<String> sheet = getSheetsBykeywordsIgnoreCase(excel, "item", "micro");
+////                System.out.println(sheet.toString());
+//                System.out.println("==========");
+//            }
+
+
+
+//        moveFileToSubFolder (proj_path,"OK",new File ("C:\\Users\\tina.yu\\Desktop\\RU\\H000019703_Biotin & Micro.xlsx"));
+//        check every subfolder under input path
+//        try {
         List<File> f = getFilesByKeywords("C:\\Users\\tina.yu\\Desktop\\RU", "");
         for (File ff : f) {
             String extension = FilenameUtils.getExtension(ff.getAbsolutePath());
             if (extension.equalsIgnoreCase("xlsx") ||
                     extension.equalsIgnoreCase("xls")) {
                 Excel excel = Excel.loadExcel(ff);
-                System.out.println(excel.getFileName());
+                String cellAddr = excel.findFirstWordInWb("test item");
+                Cell test_item = excel.getCell(cellAddr);
+                String test_item_name = excel.findfirstWordAtRight(test_item.getRowIndex(), test_item.getColumnIndex());
+           if (isMatch(test_item_name,micro_biotin_rex_pre,micro_biotin_rex_suf))
+           {
+               System.out.println(excel.getFileName());
+               System.out.println("test_item_name:" +test_item_name);
+               System.out.println("==========");
+           };
+//                FileOutputStream fileOut = new FileOutputStream(ff);
+//
+//                fileOut.flush();
+//                fileOut.close();
+//
+//                moveFileToSubFolder(proj_path,"biotin_and_Eng",ff);
 
-                List<String> sheet =getSheetsBykeywordsIgnoreCase (excel,"item","micro");
-                System.out.println(sheet.toString());
-                System.out.println("==========");
+//                List<String> sheet = getSheetsBykeywordsIgnoreCase(excel, "item", "micro");
+//                System.out.println(sheet.toString());
+
             }
 
         }
-    }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+
 
 
     public static List<File> getFilesByKeywords(String dir, String keyword) {
@@ -74,13 +123,13 @@ public class FileHandler {
         return FileName.split("\\.")[0];
     }
 
-    public static void create_save(String pathName, String content) {
+    public static File create_save(String fileAbsolutePath, String content) {
         try {
-            File theDir = new File(pathName.substring(0, pathName.lastIndexOf("/")));
+            File theDir = new File(fileAbsolutePath.substring(0, fileAbsolutePath.lastIndexOf("/")));
             if (!theDir.exists()) {
                 theDir.mkdirs();
             }
-            File file = new File(pathName);
+            File file = new File(fileAbsolutePath);
             if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
             } else {
@@ -90,10 +139,34 @@ public class FileHandler {
             myWriter.write(content);
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
+            return  file;
         } catch (
                 IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+       return null;
+    }
+
+    public static String readFromFile(String path) {
+
+        File file = new File(path);
+        return readFromFile(file);
+    }
+
+    public static String readFromFile(File file) {
+        StringBuilder result = new StringBuilder();
+        try {
+            Scanner myReader = new Scanner(file);
+            while (myReader.hasNextLine()) {
+//                result.append(myReader.nextLine().replace(" ",""));
+                result.append(myReader.nextLine());
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 }
--- src/main/java/msexcel/Excel.java
+++ src/main/java/msexcel/Excel.java
@@ -920,7 +920,66 @@ return sheetName;
 
 
     }
-    public String findBlackTitleAtLeft(int rowIdx, int colIdx) {
+    public  String findFirstWordInWb (String searchword){
+
+        int sheetSize= this.getSheetSize();
+        for (int i=0; i< sheetSize;i++){
+            this.assignSheet(i);
+            int rowSize =this.getLastRowNum();
+            for (int j=0; j< rowSize;j++){
+                this.assignRow(j);
+                int colSize =this.getLastCellNum();
+                for (int k=0; k< colSize;k++){
+                    this.assignCell(k);
+                    String cellString =this.getAbsoluteStringCellValue();
+                    if(StringUtils.containsIgnoreCase(cellString,searchword.trim())){
+                        String s =getR1C1Idx(curCell);
+                        return  s;
+                    }
+                }
+            }
+        }
+        return "";
+    }
+    public String findfirstWordAtRight(int rowIdx, int colIdx) {
+        int size = this.getLastCellNum();
+
+        for (int k = colIdx + 1; k <size; k++) {
+            this.assignRow(rowIdx);
+            this.assignCell(k);
+if (StringUtils.isNotBlank(this.getAbsoluteStringCellValue()))
+                    return this.getCellValue().toString();
+
+
+
+
+    }
+        return "";
+    }
+    public String findfirstBlackWordAtRight(int rowIdx, int colIdx) {
+    int size = this.getLastCellNum();
+        for (int k = colIdx + 1; k < size; k++) {
+            this.assignRow(rowIdx);
+            this.assignCell(k);
+            System.out.println("  rowidx:" + k);
+            XSSFCellStyle cs = ((XSSFCellStyle) this.getCurCell().getCellStyle());
+            XSSFFont font = cs.getFont();
+            XSSFColor color = font.getXSSFColor();
+            System.out.println("Font color : " + color.getARGBHex());
+            if ((this.getCellValue().toString().isEmpty()))
+                return "";
+
+            else if (!(this.getCellValue().toString().isEmpty())) {
+                if (this.getfontColor().equals("NULL") || this.getfontColor().equals("FF000000")) {
+                    System.out.println("if loop" + k);
+                    return this.getCellValue().toString();
+
+                }
+            }
+        }
+        return "";
+    }
+    public String findfirstBlackWordAtLeft(int rowIdx, int colIdx) {
         for (int k = colIdx - 1; k >= 0; k--) {
             this.assignRow(rowIdx);
             this.assignCell(k);
@@ -943,7 +1002,7 @@ return sheetName;
         return "";
     }
 
-    public String findBlackTitleAtTop(int rowIdx, int colIdx) {
+    public String findFirstBlackWordAtTop(int rowIdx, int colIdx) {
         for (int k = rowIdx - 1; k >= 0; k--) {
             this.assignRow(k);
             this.assignCell(colIdx);
